a13cd38d2115942f58982d70c8cd9e25564de639
jooony <shrimp.lad@gmail.com>
2022-12-08 14:54:48 +0000

    added turret script

--- Assets/TurretScript.cs
+++ Assets/TurretScript.cs
@@ -6,6 +6,7 @@ public class TurretScript : MonoBehaviour
 {
     float maxRotation = 100;
     float minRotation = 20;
+    float turretTurnSpeed = 10;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,10 +20,21 @@ public class TurretScript : MonoBehaviour
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector3 direction = mousePosition - transform.position;
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        float currentRotation = transform.rotation.z;
+
+        if (angle < currentRotation)
+        {
+       //     angle = currentRotation - (turretTurnSpeed*Time.deltatime);
+        }
+
+
+        // the turret has a maximum and minimum rotation
         if (angle < minRotation)
             angle = minRotation;
         if (angle > maxRotation)
             angle = maxRotation;
+        
+
         transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
     }
 }
